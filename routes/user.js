473406221f4b21b64d47d4cf9789e7191c69f1d0
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2023-05-11 18:53:53 +0700

    add authorizationUser need fix

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
 const user = require('../controllers/userController')
+const authentication = require("../middleware/authentication")
+const authorizationUser = require('../middleware/authorizationUser')
 
-router.get('/', user.getAllUsers)
-router.put('/:id', user.updateUser)
 router.post('/register', user.register)
 router.post('/login', user.login)
+router.use(authentication)
+router.get('/', user.getAllUsers)
+router.put('/:id',authorizationUser, user.updateUser)
 router.delete('/:id', user.deleteUser)
 
 
