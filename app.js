1bd80e38278a96a2a31b49949baeb7615cad88d2
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2023-05-14 16:14:48 +0700

    start

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
 const router = require('./routes')
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
